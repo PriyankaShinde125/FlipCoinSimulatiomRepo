f7d83d8cc5e14c1eb69c6ab8d3dc2c4cdbd55f19
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 16:57:34 +0530

    [Priyanka] uc2 multiple toss

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,20 @@
 #! /bin/bash -x
 
 isHead=1
+headWon=0
+tailWon=0
+echo "enter number of times do you want to play"
+read n
+for ((i=0;i<n;i++))
+do
 toss=$((RANDOM%2))
 if [ $toss -eq $isHead ]
 then
-	echo "Head wins"
+	((headWon++))
 	else
-	echo "tail wins"
+	((tailWon++))
 fi
+done
+
+echo "head won $headWon times"
+echo "tail won $tailWon times"
